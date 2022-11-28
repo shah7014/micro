33d0a2b25a58834a935a3e9721df8cc2b70e96ec
Romil Shah <romilshah2199@gmail.com>
2022-11-28 13:14:14 +0530

    container deployemtn script

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -1,7 +1,5 @@
 const { merge } = require("webpack-merge");
 
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 const commonConfig = require("./webpack.common");
@@ -23,9 +21,6 @@ const devConfig = {
         marketing: "marketing@http://localhost:8081/remoteEntry.js",
       },
       shared: packageJson.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
     }),
   ],
 };
